b7660a785b1123c1c3946d7047efc5249a87b34a
narahsouza <narahnsa@gmail.com>
2022-12-06 21:00:25 -0300

    Update

---
+++ Codigo Java/Pag_039-40/Matematica.java
@@ -0,0 +1,12 @@
+// Página 39 - Código 4. Classe Matematica com atributo.
+
+public class Matematica {
+
+	// AQUI FICARÃO OS ATRIBUTOS DA CLASSE
+	int resultado;
+
+	// AQUI FICARÃO OS MÉTODOS DA CLASSE
+	public void soma(int valor1, int valor2) {
+		resultado=valor1+valor2;
+	}
+}
---
+++ Codigo Java/Pag_040/Matematica.java
@@ -0,0 +1,17 @@
+// Página 39 - Código 4. Classe Matematica com atributo.
+
+public class Matematica {
+
+	// AQUI FICARÃO OS ATRIBUTOS DA CLASSE
+	int resultado;
+
+	// AQUI FICARÃO OS MÉTODOS DA CLASSE
+	public void soma(int valor1, int valor2) {
+		resultado=valor1+valor2;
+	}
+
+	public void multiplica(int valor1, int valor2) {
+		resultado=valor1*valor2;
+	}
+	
+}
