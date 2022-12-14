d19b3e08344ed2a22b50c7d7c9266df728ede6a6
narahsouza <narahnsa@gmail.com>
2022-12-13 21:58:36 -0300

    Update

---
+++ TestesMatematicos/src/MatematicaAvancada.java
@@ -0,0 +1,19 @@
+// Figura 31. Código inicial de MatematicaAvancada, com erro.
+// Figura 33. Resultado da primeira ajuda do Eclipse.
+// Figura 36. Setter e Getter criado automaticamente pelo Eclipse.
+
+public class MatematicaAvancada {
+	private double resultado;
+
+	public void soma(double valor1, double valor2) {
+		setResultado(valor1*valor2);
+	}
+
+	public double getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(double resultado) {
+		this.resultado = resultado;
+	}
+}
---
+++ TestesMatematicos/src/TestaSoma.java
@@ -0,0 +1,11 @@
+
+public class TestaSoma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MatematicaAvancada ma=new MatematicaAvancada();
+		ma.soma(10, 5);
+		System.out.println("Resultado: "+ma.getResultado());
+	}
+}
