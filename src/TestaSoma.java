27736025cfa9124f5a9a7d590848624e8f598199
narahsouza <narahnsa@gmail.com>
2022-12-28 19:41:56 -0300

    Update

---
+++ src/TestaSoma.java
@@ -0,0 +1,12 @@
+// Página 60 - Figura 38. Criando classe com ponto de início para execução.
+
+public class TestaSoma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MatematicaAvancada ma=new MatematicaAvancada();
+		ma.soma(10, 5);
+		System.out.println("Resultado: "+ma.getResultado());
+	}
+}
