d0dd64acc23a1d0e8a450d323577d7b3075d8c3f
narahsouza <narahnsa@gmail.com>
2022-12-29 12:01:20 -0300

    Update

--- src/MatematicaAvancada.java
+++ src/MatematicaAvancada.java
@@ -1,6 +1,7 @@
-// Figura 31. Código inicial de MatematicaAvancada, com erro.
-// Figura 33. Resultado da primeira ajuda do Eclipse.
-// Figura 36. Setter e Getter criado automaticamente pelo Eclipse.
+// Página 55 - Figura 31. Código inicial de MatematicaAvancada, com erro.
+// Página 57 - Figura 33. Resultado da primeira ajuda do Eclipse.
+// Página 58 - Figura 36. Setter e Getter criado automaticamente pelo Eclipse.
+// Página 66 - Código 14. Classe MatematicaAvancada com método par().
 
 public class MatematicaAvancada {
 	private double resultado;
@@ -15,5 +16,17 @@ public class MatematicaAvancada {
 
 	public void setResultado(double resultado) {
 		this.resultado = resultado;
+	}
+	
+	public boolean par(double valorDec) { 
+		
+		boolean retorno = false; 
+		
+		int valorInt = (int) valorDec; 
+		
+		if (valorDec/2==valorInt/2) retorno=true; 
+		
+		return retorno; 
+		
 	}
 }
--- src/TestaSoma.java
+++ src/TestaSoma.java
@@ -1,12 +1,22 @@
 // Página 60 - Figura 38. Criando classe com ponto de início para execução.
+// Página 67 - Código 15. Classe TestaSoma atualizada para testar o método par().
 
 public class TestaSoma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		MatematicaAvancada ma=new MatematicaAvancada();
 		ma.soma(10, 5);
-		System.out.println("Resultado: "+ma.getResultado());
+		
+		// System.out.println("Resultado: "+ma.getResultado());
+		
+		double rsto= ma.getResultado(); 
+		System.out.println("Resultado: "+rsto); 
+		
+		if(ma.par(rsto)) { 
+			System.out.println("Este número é par"); 
+			
+		} else { 
+			System.out.println("Este número é ímpar"); }
 	}
 }
